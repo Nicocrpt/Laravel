8832a571a952e78d576b44adaec01cf2fed65acb
Nicolas Carpita <nicolascarpita@Nicolass-MacBook-Pro.local>
2024-06-27 11:52:54 +0200

    créations migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('prenom', 70);
+            $table->date('date_naissance');
+            $table->string('adresse');
+            $table->string('code_postal', 50);
+            $table->string('ville', 150);
+            $table->string('telephone', 70);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
---
+++ database/migrations/2024_06_27_094103_create_inscriptions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->string('cours', 150);
+            $table->string('eleve', 70);
+            $table->timestamps();
+            $table->foreignId('eleve_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('cours_id')->constrained('cours')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inscriptions');
+    }
+};
